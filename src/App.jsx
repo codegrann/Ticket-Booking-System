c446a00904aa30568b8fcd36ef7394405656d1c3
codegrann <omondihills02@gmail.com>
2024-02-27 22:23:21 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 import Client from "./pages/Client";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
@@ -13,6 +16,7 @@ function App() {
           <Route path="/profile" element={<Client />} />
         </Routes>
       </Router>
+      <Footer />
     </div>
   );
 }
