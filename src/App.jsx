d965c7c2ee3116e0fbc13a5deb2082b6b17c48f2
codegrann <omondihills02@gmail.com>
2024-02-27 16:59:17 +0300

    setup m routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import Client from "./pages/Client";
 
 function App() {
-  return <p className="bg-red-900">Welcome aboard app.jsx</p>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/profile" element={<Client />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
