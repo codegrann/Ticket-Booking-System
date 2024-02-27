a8dfa039dd4884636c11e09c7e18dc51d6aa66c1
codegrann <omondihills02@gmail.com>
2024-02-27 05:16:59 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>Welcome aboard</>;
+  return <p>Welcome aboard</p>;
 }
 
 export default App;
