b4431485bb984ca8e9ae8385d734ed94c973d15c
codegrann <omondihills02@gmail.com>
2024-02-28 08:50:32 +0300

    .

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -50,7 +50,7 @@ function EventList() {
       <h3 className="text-black font-medium md:text-xl mt-6">
         Upcoming Events
       </h3>
-      <div>
+      <div className="flex flex-col gap-4 flex-wrap md:flex-row">
         {events.map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
