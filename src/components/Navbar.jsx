22e24f3be5a6c477b300f2f1cc5071b0f4a45549
codegrann <omondihills02@gmail.com>
2024-02-28 07:10:35 +0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
+import "./Navbar.css";
 // import Logo from "./Minor/Logo";
 // import "../App.css";
 // import ImageContainer from "./Minor/ImageContainer";
@@ -18,40 +19,14 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container ">
-        <div className="logo">
-          <Link to="/">{/* <Logo /> */}</Link>
-        </div>
+        <div className="logo">{/* <Link to="/"><Logo /></Link> */}</div>
         <div className="menu-icon" onClick={handleShowNavbar}>
           <Hamburger />
-          {/* <p>menu icon</p> */}
+          <p>menu icon</p>
         </div>
         <div className={`nav-elements  ${showNavbar && "active"} `}>
           <ul>
-            <li className="hover">
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <a href="/#aboutus">About us</a>
-              {/* <Link to="/#aboutus">About us</Link> */}
-            </li>
-            <li>
-              <a href="/#services" className="">
-                Services
-              </a>
-              {/* <Link to="#services">Services</Link> */}
-            </li>
-            <li>
-              <a href="/#contactus" className="">
-                Contact
-              </a>
-              {/* <Link to="/#contactus">Contact</Link> */}
-            </li>
-            <li id="getstarted" className="btn btn-success btn-outline ">
-              <span className="font-semibold">Get started</span>
-            </li>
-            <li className="border-2 border-red-400 cursor-pointer w-16 md:w-8">
-              {/* <ImageContainer imageurl="/person.png" size={10} /> */}
-            </li>
+            <li>item 1</li>
           </ul>
         </div>
       </div>
