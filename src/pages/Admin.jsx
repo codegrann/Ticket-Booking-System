1719281b2c612d48b3c2b331ab346a7b5911536c
codegrann <omondihills02@gmail.com>
2024-02-29 10:55:11 +0300

    .

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import EventList from "../components/EventList";
 
 function Admin({ events }) {
+  const [addClicked, setAddClicked] = useState(false);
+  const addEventToList = () => {
+    setAddClicked(true);
+  };
   return (
     <div className="my-8">
       <h3 className="text-lg font-semibold">Welcome Administrator</h3>
       <p className="text-xs font-semibold">Manage your events with ease</p>
-      <button className="btn btn-primary btn-outline">Add event</button>
+      <button className="btn btn-primary btn-outline" onClick={addEventToList}>
+        Add event
+      </button>
+      {addClicked && (
+        <form>
+          <input type="text" />
+        </form>
+      )}
       <EventList events={events} />
     </div>
   );
