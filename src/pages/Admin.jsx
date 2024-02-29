e31d953a8c12bd51614260cd7dc92a317b086f92
codegrann <omondihills02@gmail.com>
2024-02-29 12:27:47 +0300

    .

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -13,18 +13,32 @@ function Admin({ events }) {
       <button className="btn btn-primary btn-outline" onClick={addEventToList}>
         Add event
       </button>
-      {addClicked && (
-        <>
-          <button onClick={() => setAddClicked(false)} className="ml-4">
-            Cancel
-          </button>
-          <form>
-            <input type="text" placeholder="enter event name" />
-            <input type="text" placeholder="enter event date" />
-            <input type="text" placeholder="enter event location" />
-          </form>
-        </>
-      )}
+      {/* {addClicked && ( */}
+      <>
+        <button
+          onClick={() => setAddClicked(false)}
+          className="btn btn-error ml-4"
+        >
+          Cancel
+        </button>
+        <form>
+          <div className="flex items-center m-auto w-full bg-[#9dc69d] rounded-md ">
+            <input
+              className="caret-red-700 py-4 bg-transparent input input-sm md:py-6  w-full max-w-xs md:input-md md:max-w-xl focus:outline-none"
+              // className="w-[400px] h-[50px] bg-transparent border-none outline-none text-[#797979] text-md"
+              type="email"
+              placeholder="Email"
+              required
+              // value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <input type="text" placeholder="enter event name" />
+          <input type="text" placeholder="enter event date" />
+          <input type="text" placeholder="enter event location" />
+        </form>
+      </>
+      {/* )} */}
       <EventList events={events} />
     </div>
   );
