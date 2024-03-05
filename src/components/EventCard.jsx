33ee2a28da82e0ab059b65ca67750e9110ea862b
codegrann <omondihills02@gmail.com>
2024-03-05 10:02:43 +0300

    .

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -42,12 +42,10 @@ function EventCard({
     updatedTickets[index][key] = value;
     setTickets(updatedTickets);
   };
-  const position = isEditing
-    ? " md:bg-white md:z-10 md:fixed md:top-1/4 md:left-1/4"
-    : "md:relative";
+  const position = isEditing ? " bg-white z-10 " : "";
   return (
     <div
-      className={`border-2  rounded-lg shadow-lg p-4 flex flex-col gap-4 min-w-[290px] max-w-[435px] ${position}`}
+      className={`border-2 relative rounded-lg shadow-lg p-4 flex flex-col gap-4 min-w-[290px] max-w-[435px] ${position}`}
     >
       {role === "ADMIN" && (
         <span
